29b7d881e7dc4700cc0f71ad05b5d230930cb0e7
PriscaRevalina <143679451+PriscaRevalina@users.noreply.github.com>
2023-09-30 21:34:33 +0700

    Telah menyelesaikan tugas

--- P02_01.java
+++ P02_01.java
@@ -1,1 +1,42 @@
-// NIM - Your Name
+// 12S23039-Prisca Manurung
+import java.util.*;
+import java.lang.Math;
+
+class P02-01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String namaTugas, kodeMatkul, mataKuliah, dosenPengampu, deadline, nIMNamaMatkul, status, rekomendasiTugas;
+        int tingkatKesulitan, hari;
+        double prioritas;
+
+        namaTugas = input.nextLine();
+        kodeMatkul = input.nextLine();
+        mataKuliah = input.nextLine();
+        dosenPengampu = input.nextLine();
+        deadline = input.nextLine();
+        nIMNamaMatkul = input.nextLine();
+        status = input.nextLine();
+        tingkatKesulitan = input.nextInt();
+        rekomendasiTugas = input.nextLine();
+        hari = input.nextInt();
+        prioritas = Tingkat * (1.0 / hari);
+        System.out.println("Prioritas: " + toFixed(prioritas,2));
+        if (prioritas > 3) {
+            Hasil1 = "Penting! Anda harus mengerjakan tugas ini segera";
+        } else {
+            if (prioritas >= 1.5 && prioritas <= 3) {
+                Hasil1 = " Tugas ini memiliki prioritas tingkat menengah";
+            } else {
+                if (prioritas <= 1.5) {
+                    Hasil1 = "Tugas ini relatif ringan, namun jangan tunda terlalu lama.";
+                }
+            }
+        }
+        System.out.println(Nama + " | " + Kode + " | " + mataKuliah + " | " + Dosen + " | " + deadline + " | " + NIM + " | " + Tingkat + " | " + hari + " | " + status);
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
--- P02_02.java
+++ P02_02.java
@@ -1,1 +1,34 @@
-//NIM -Nama
+//12S23039-Prisca Manurung
+import java.util.*;
+import java.lang.Math;
+
+class P02-02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String namagudang, aC, lantai, ketersediaanteknologi, kualitasGudang;
+        int kapasitasgudang, jumlahbukusaatini, skorGudang;
+
+        namagudang = input.nextLine();
+        kapasitasgudang = input.nextInt();
+        jumlahbukusaatini = input.nextInt();
+        aC = input.nextLine();
+        lantai = input.nextLine();
+        ketersediaanteknologi = input.nextLine();
+        skorGudang = (int) ((double) jumlahbukusaatini / kapasitasgudang * 100);
+        if (skorGudang >= 40) {
+            kualitasGudang = "Gudang Elite";
+        } else {
+            if (skorGudang >= 25) {
+                kualitasGudang = "Gudang Standar";
+            } else {
+                if (skorGudang < 25) {
+                    kualitasGudang = "Gudang Perlu Peningkatan";
+                }
+            }
+        }
+        System.out.println("Informasi Gudang");
+        System.out.println(namagudang + " | " + kapasitasgudang + " | " + jumlahbukusaatini + " | " + aC + " | " + lantai + " | " + ketersediaanteknologi + " | " + skorGudang + " | " + kualitasGudang);
+    }
+}
+
